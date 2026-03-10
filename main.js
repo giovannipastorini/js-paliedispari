e863824c28dc8f6d57700088a26e6e28250ee1f5
giovannipastorini <giovannipastorini9@gmail.com>
2026-03-10 05:34:27 +0100

    task2: control the user inputs and generate a random numebr

--- main.js
+++ main.js
@@ -2,15 +2,7 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 
-//L'utente inserisce una parola
-const wordA= prompt("Inserisci una parola");
-
-//chiamo la funzione  "IsPalindrome"
-const checkPal= IsPalindrome(wordA);
-console.log(checkPal);
-
-
-//creo e dichiaro una funzione di nome "IsPalindrome" per verificare se una parola è palindroma
+//dichiaro una funzione di nome "IsPalindrome" per verificare se una parola è palindroma
 function IsPalindrome(stringa){
     const l=stringa.length;
     const stringaArray=[];
@@ -35,16 +27,12 @@ function IsPalindrome(stringa){
     console.log(stringaArray);
 
     //verifico l'uguaglianza dei due array:
-    //Se trovo sempre corrispondenza fra gli elementi degli array che si trovano nella stessa posizione, allora:
-    //la parola è palindroma!
-    /* let IsPal= null; */
-    
-
     for(let i=0; i< stringaArray.length; i++){
         console.log(stringaArray[i]);
         console.log(stringaArrayReverse[i]);
-        
-        if(stringaArray[i] != stringaArrayReverse[i]){
+        //Se trovo sempre corrispondenza fra gli elementi degli array che si trovano nella stessa posizione, allora:
+        //la parola è palindroma!
+        if(stringaArray[i] !== stringaArrayReverse[i]){
             //LA PAROLA NON E' PALINDROMA
             /* IsPal= false; */
             return ("la parola non è palindroma");
@@ -55,8 +43,53 @@ function IsPalindrome(stringa){
    
 }
 
+//L'utente inserisce una parola
+const wordA= prompt("Inserisci una parola");
+
+//chiamo la funzione  "IsPalindrome"
+const checkPal= IsPalindrome(wordA);
+console.log(checkPal);
+
+//TASK 2: Pari e Dispari
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer (metodo js random (numeri casuali)).
+//Sommiamo i due numeri
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Dichiariamo chi ha vinto.
+
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+const evenOdd= prompt("Inserisci pari o dispari");
+
+//controllo se l'utente ha inserito un valore corretto
+if (evenOdd==="pari" || evenOdd==="dispari"){
+    //l'utente inserisce un numero da 1 a 5 
+    const userNum=parseInt(prompt ("Inserisci un numero da 1 a 5"));
+    if (userNum>=1 && userNum <=5){
+        console.log("hai inserito un numero giusto");
+        
+        //Generiamo un numero random (sempre da 1 a 5) per il computer (metodo js random (numeri casuali)).
+        const pcNum= Math.floor((Math.random() * 5) + 1);
+        console.log(pcNum);
+        
+
+
+    }else{
+        console.log("hai inserito un numero sbagliato");
+    }
+}else{
+    console.log("hai inserito un valore sbagliato");
+}
 
 
 
 
-//metodo js random (numeri casauli)
+
+
+
+
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 Esercizio di oggi:
 nome repo: js-paliedispari
 
-Palidroma
+<!-- Palidroma
 Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma
+Creare una funzione per capire se la parola inserita è palindroma -->
 
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
